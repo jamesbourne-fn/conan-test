ed8491eb4899fe6c3eb30a799aa9fa2fbef6b0f3
jamesbourne-fn <58814211+jamesbourne-fn@users.noreply.github.com>
2020-04-07 09:49:43 +0100

    added doxygen comments
    added doxygen comments to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
 #include <string>
 
+/**
+ * This is a utility method to print to output
+ * to a stream
+ * @param os This is the output stream
+ * @param msg This is the message to print
+ */
 void print(std::ostream& os, std::string msg)
 {
     os << msg << std::endl;
 }
 
-
+/** 
+ *  This is the main method
+ */
 int main(int argc, char *argv[]) 
 {
     print(std::cout, "Hello World!!");
